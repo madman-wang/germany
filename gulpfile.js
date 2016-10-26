ad60a7e251c37c27b4006c37f50116795f7765c4
wangyi <1159294999@qq.com>
2016-10-26 18:21:06 +0800

    字体 margin padding 转适配px

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('pxrem', function() {
 
 px2rem({
     baseDpr: 2,             // 基于设备的Dpr
-    threeVersion: true,    // 是否生成 @1x, @2x and @3x 三个版本(默认: false)，打开后
+    threeVersion: false,    // 是否生成 @1x, @2x and @3x 三个版本(默认: false)，打开后
     remVersion: true,       // 是否生成rem版本(默认: true)
     remUnit: 64,            // rem转换比例 (默认: 75)
     remPrecision: 5         // rem保留几位数 (默认: 5)
